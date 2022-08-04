867e41145591e0f7a31aae7c2dbfded92354839d
guitischer <guitischer@hotmail.com.br>
2022-08-03 23:21:38 -0300

    added annotation at poll repo

--- backend/src/main/java/com/desafio/springboot/repositories/PollRepository.java
+++ backend/src/main/java/com/desafio/springboot/repositories/PollRepository.java
@@ -1,9 +1,11 @@
 package com.desafio.springboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.desafio.springboot.models.Poll;
 
+@Repository
 public interface PollRepository extends JpaRepository<Poll, Long>{
   
 }
