3290d0cf3c19494e3b56a1a8aa6e5b2257c43997
guitischer <guitischer@hotmail.com.br>
2022-08-05 23:26:44 -0300

    added default value to endAt field of poll

--- backend/src/main/java/com/desafio/springboot/services/PollService.java
+++ backend/src/main/java/com/desafio/springboot/services/PollService.java
@@ -1,5 +1,9 @@
 package com.desafio.springboot.services;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +17,12 @@ public class PollService {
   PollRepository pollRepository;
 
   public void createPoll(Poll poll) {
+
+    if (poll.getEndAt() == null) {
+      LocalDateTime today = LocalDateTime.now().plus(Duration.of(1, ChronoUnit.MINUTES));
+      poll.setEndAt(today);
+    }
+
     pollRepository.save(poll);
   }
 }
