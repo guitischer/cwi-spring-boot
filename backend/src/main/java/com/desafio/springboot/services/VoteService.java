cea70ce8e061777a496a2a0cd173c1aadce8a573
guitischer <guitischer@hotmail.com.br>
2022-08-16 20:28:31 -0300

    remove unused imports

--- backend/src/main/java/com/desafio/springboot/services/VoteService.java
+++ backend/src/main/java/com/desafio/springboot/services/VoteService.java
@@ -1,14 +1,11 @@
 package com.desafio.springboot.services;
 
-import java.util.List;
-
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.desafio.springboot.dtos.VoteDTO;
 import com.desafio.springboot.exceptions.MissingParameterException;
-import com.desafio.springboot.models.Poll;
 import com.desafio.springboot.models.Vote;
 import com.desafio.springboot.repositories.VoteRepository;
 
