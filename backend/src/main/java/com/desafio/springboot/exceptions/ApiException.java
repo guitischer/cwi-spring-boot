9cf63fbd7a7a708b6fb3be636b475dec574c5e3a
guitischer <guitischer@hotmail.com.br>
2022-08-16 23:26:52 -0300

    added cpf validation when vote

---
+++ backend/src/main/java/com/desafio/springboot/exceptions/ApiException.java
@@ -0,0 +1,22 @@
+package com.desafio.springboot.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
+public class ApiException extends RuntimeException {
+
+  private String message;
+
+  public ApiException(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+}
