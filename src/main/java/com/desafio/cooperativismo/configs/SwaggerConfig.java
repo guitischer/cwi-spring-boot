6d8e6ad799c3309c98c413382e1e933e19235ef2
guitischer <guitischer@hotmail.com.br>
2022-08-20 20:30:45 -0300

    Update SwaggerConfig.java

--- src/main/java/com/desafio/cooperativismo/configs/SwaggerConfig.java
+++ src/main/java/com/desafio/cooperativismo/configs/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
         .select()
-        .apis(RequestHandlerSelectors.any())
+        .apis(RequestHandlerSelectors.basePackage("com.desafio.cooperativismo"))
         .paths(PathSelectors.any())
         .build()
         .apiInfo(apiInfo());
